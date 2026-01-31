304d5828da7a7372af8ade890bc65f38854c58c4
toshokarpachev <toshokarpachev@gmail.com>
2026-01-31 21:14:08 +0200

    Add-section-styles

--- App.js
+++ App.js
@@ -5,41 +5,69 @@ import { SafeAreaView, SafeAreaProvider } from 'react-native-safe-area-context';
 export default function App() {
   return (
     <SafeAreaProvider>
-    <SafeAreaView style={styles.container}>
-      {/* Header*/}
-      <View>
-        <Text>Calorie Tracker</Text>
+      <SafeAreaView style={styles.container}>
+        {/* Header*/}
+        <View style={[styles.section, styles.header]}>
+          <Text style={styles.heading}>Calorie Tracker</Text>
 
-      </View>
+        </View>
 
-      {/* Overview*/}
-      <View>
-        <Text>Calorie Overview</Text>
+        {/* Overview*/}
+        <View style={styles.section}>
+          <Text>Calorie Overview</Text>
 
-      </View>
-      {/* Meal Section */}
-      <View>
-        <Text>List of Meals</Text>
+        </View>
+        {/* Meal Section */}
+        <View style={styles.section}>
+          <Text>List of Meals</Text>
 
-      </View>
-      {/* App Bar */}
-      <View>
-        <Text>App Bar</Text>
+        </View>
+        {/* App Bar */}
+        <View style={styles.section}>
+          <Text>App Bar</Text>
 
-      </View>
+        </View>
 
 
-      <StatusBar style="auto" />
-    </SafeAreaView>
+        <StatusBar style="auto" />
+      </SafeAreaView>
     </SafeAreaProvider>
   );
+}
+
+const colors = {
+  white: '#fff',
+  background:  '#dedede',
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: colors.background,
     alignItems: 'center',
     justifyContent: 'space-between',
   },
+
+  section: {
+     boxShadow: [{offsetX:5, offsetY:5, blurRadius:10, color:'#0005'}],
+
+    backgroundColor: colors.white,
+    borderColor: '#a7a7a7',
+    borderStyle: 'solid',
+    borderRadius:10,
+    borderWidth: 1,
+    width:'90%',
+    padding: 20,
+
+  },
+
+  header: {
+   
+   
+  },
+  heading: {
+    textAlign:'center',
+    fontSize:26,
+    fontWeight:'bold',
+  }
 });
